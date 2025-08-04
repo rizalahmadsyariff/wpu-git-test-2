8e6d4d54ed8357683ae58aca4e2b164795ea52c9
rizalahmadsyariff <rizalahmadsyarif@gmail.com>
2025-08-04 19:22:16 +0800

    menambahkan paragrap

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident perferendis beatae fuga accusantium numquam iure ab laudantium incidunt sed dolorem repellendus, id minima assumenda perspiciatis tempore quis quidem voluptas delectus.</p>
 </body>
 </html>
